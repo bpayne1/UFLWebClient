f3a7f584d8c1d18898c797f5e411c3331b4ff054
bpayne1 <bpayne@osisoft.com>
2015-12-30 10:29:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFLWebClient
+{
+    class Program
+    {
+        static string uri = "https://server:port/connectordata/Webservice"; //Address from UFL Connector Datasource configuration
+
+        static void Main(string[] args)
+        {
+            string data = "servicetest,72.00," + DateTime.Now.ToString("dd-MMM-yyyy HH:mm:ss");
+            Console.WriteLine("Data to send: " + data);
+            byte[] fileToSend = Encoding.UTF8.GetBytes(data);
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(
+               (object sender, X509Certificate certification, X509Chain chain, SslPolicyErrors sslPolicyErrors) => { return true; });
+            using (var wb = new WebClient())
+            {
+                var responsess = wb.UploadData(@uri, "PUT", fileToSend);
+                string responses = Encoding.ASCII.GetString(responsess);
+                Console.WriteLine(responses);
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("ReadLine");
+        }
+    }
+}
